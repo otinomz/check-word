6d29f1a70ef1761ae93c80b91662665bf3639740
otinomz <awesomeotinomz@gmail.com>
2022-01-12 13:52:02 +0100

    fetching data with axios

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import './App.css';
+import axios from 'axios'
+
+const BASE_URL = "https://api.dictionaryapi.dev/api/v2/entries/en/plane"
 
 function App() {
-  
+
+  const dictionaryAPI = async () => {
+    try {
+      const data = await axios.get(
+        BASE_URL
+      )
+    } catch (error){
+      console.log(error)
+    }
+  }
+
+
+
   return (
     <div className="App">
       Dictionary App using the dictionary API
