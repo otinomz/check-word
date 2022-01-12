850a9dba14763891449c031c895d91a594a6252f
otinomz <awesomeotinomz@gmail.com>
2022-01-12 18:09:00 +0100

    new changes

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -3,6 +3,13 @@ import React from 'react'
 const Definitions = ({word, meanings, category}) => {
     return (
         <div>
+            {
+                word === "" ? (
+                    <span className="subTitle">Start By Typing A Word In Search</span>
+                ) : (
+                    "something"
+                )
+            }
         </div>
     )
 }
