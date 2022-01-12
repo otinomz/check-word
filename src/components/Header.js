7c62b1f0e53e98e68825825a659b0286c55ff95a
otinomz <awesomeotinomz@gmail.com>
2022-01-12 15:20:14 +0100

    more and more and more

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,6 +5,10 @@ const Header = () => {
     return (
         <div className="header">
             <span className="title">Word Hunt</span>
+
+            <div className="inputs">
+                
+            </div>
         </div>
     )
 }
