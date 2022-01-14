615c165a77c16f198fc8401473692c6ba5f74379
otinomz <awesomeotinomz@gmail.com>
2022-01-14 23:12:00 +0100

    new changes

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -5,6 +5,8 @@ const Definitions = ({word, meanings, category}) => {
     return (
         <div className="meanings">
             {
+                // rendering the audio at the top for 
+                // english category alone
                 meanings[0] && word && category === "en" && (
                     <audio
                         src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio}
