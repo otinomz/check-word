c24ccf1b6de2c0316e6927326262596319f82404
otinomz <awesomeotinomz@gmail.com>
2022-01-14 23:09:08 +0100

    new changes

--- src/components/Definitions/Definitions.js
+++ src/components/Definitions/Definitions.js
@@ -6,7 +6,7 @@ const Definitions = ({word, meanings, category}) => {
         <div className="meanings">
             {
                 meanings[0] && word && category === "en" && (
-                    <audio src={meanings[0].phonetics[0]} style={{backgroundColor: "#fff", borderRadius: 10}}>
+                    <audio src={meanings[0].phonetics[0] && meanings[0].phonetics[0].audio} style={{backgroundColor: "#fff", borderRadius: 10}}>
                         Your Browser doesn't support audio element
                     </audio>
                 )
